ddbe0fbf13b555a879c5adac0738102d4b4cf531
king <1063465766@qq.com>
2018-08-23 23:10:42 +0800

    add java db test

--- src/test/java/com/wantdo/mybatisdemo/MybatisDemoApplicationTests.java
+++ src/test/java/com/wantdo/mybatisdemo/MybatisDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.wantdo.mybatisdemo;
 
+import com.wantdo.mybatisdemo.entity.User;
 import com.wantdo.mybatisdemo.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +16,15 @@ public class MybatisDemoApplicationTests {
     
     @Test
     public void contextLoads() {
+        try {
+            User user = User.class.newInstance();
+            user.setPassword("dshjgdfkjs");
+            System.out.println(user.getPassword());
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
     
     @Resource
